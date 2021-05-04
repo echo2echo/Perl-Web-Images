2057e0a51a0334862fb5341188bf417b777ef778
Joel Rader <echophotographic@gmail.com>
2021-05-04 13:56:35 -0400

    Added image file extension case additions

--- perl-cgi-images.pl
+++ perl-cgi-images.pl
@@ -20,13 +20,13 @@ my $end = 0;
 my $start = 0;
 my $element = 0;
 
-# Declare @imagehtml which will later hold array of html for images placed in public folder...
+# Declare @imagehtml which will later hold array of html for images placed in public folder...    
 my @imagehtml;
 
 # Place your images (jpg jpeg and png ok here) in a local directory that has public http access
 # Next we will "find" all the "jpg", "JPG", "jpeg" or "png" images in your public folder containing your images.  Images to be displayed will be sorted alphanumerically. 
 # Arrange photo file names alphanumerically to determine display order when running this script
-my @find = `find /var/www/YourSite.net/public_html/YourDirectory . -name "*.jpg" -o -name "*.JPG" -o -name "*.jpeg" -o -name "*.png" | sort --ignore-case`;
+my @find = `find /var/www/YourSite.net/public_html/YourDirectory . -name "*.jpg" -o -name "*.JPG" -o -name "*.jpeg" -o "*.JPEG" -o -name "*.png" -o "*.PNG" |  sort --ignore-case`;
 
 print "Content-Type: text/html\n\n";
 print "<!DOCTYPE html>\n";
