413b72c497d4bf6b2f1ef2f5a67f8b072de48e9a
Joel Rader <echophotographic@gmail.com>
2021-05-10 19:26:54 -0400

    Create resize_images.pl
    First commit of thumbnail generator

---
+++ resize_images.pl
@@ -0,0 +1,60 @@
+#!/usr/bin/env perl
+
+# File name: resize_image.pl
+# Perl script to resize jpg and png images
+# By Joel Rader  info[at]joelrader[dot]net
+# Create resized images of files (jpg jpeg png) with size of your choice
+# The script works as a thumbnail generator
+
+use strict;
+use warnings;
+use diagnostics;
+
+my @find = `find . -name "*.jpg" -o -name "*.JPG" -o -name "*.jpeg" -o -name "*.JPEG" -o -name "*.png" -o -name "*.PNG" |  sort --ignore-case`;
+
+my $thumb;
+my $fileSize;
+my $fileName;
+
+foreach (@find) {
+
+        $fileName = $_;
+
+        chomp $fileName;
+
+        $fileSize = -s $fileName;
+
+        # if the "thumb_" file is already there go to next in array     
+        if ( $fileName =~ m/thumb_/g ) { print "Skip thumb $fileName\n"; next; }
+
+        # if the file size is greater than 1 MB continue on and resize image
+
+        # Change the following value of $bigfile to the size of the image files you would like to replace
+        # 1 MB is used here...
+        my $bigfile = 1000000;
+
+        if ( $fileSize > $bigfile ) {
+
+                # If you do not want to include resizing images from subdirectory modify the following
+                # change foldernametoskip on the next line 
+                if ( $fileName =~ m/foldernametoskip/ ) { print "Skip imgbak folder $fileName\n"; next; }
+
+                resize();
+
+                print "Made thumb from $fileName\n";
+        }
+}
+
+
+sub resize {
+
+        $fileName =~ s/\.\///;
+
+        # You may need to install imagemagick. 
+        # On Ubuntu linux try the following...
+        # $ sudo apt install imagemagick 
+        # or build from source... see  https://imagemagick.org for instructions
+        # This command will overwrite the existing thumb images
+        $thumb = `convert $fileName -resize x400 thumb_$fileName 2>/dev/null`;
+
+}
