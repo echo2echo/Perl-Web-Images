1b1a0724a87505f1b1f6f7e4035f1282266fc07c
Joel Rader <echophotographic@gmail.com>
2021-05-23 22:23:49 -0400

    Update perl-cgi-images.pl
    removed 10 sheets per page liimit note

--- perl-cgi-images.pl
+++ perl-cgi-images.pl
@@ -25,8 +25,7 @@ my $originalPath;
 my @imagehtml;
 
 # Place your images ( jpg jpeg and png ) in a local directory that has public http access
-# This script works best aesthetically if there are 60 or fewer images in a gallery. Though it is your call.
-# Next we will "find" all the "jpg", "JPG", "jpeg" or "png" images in your public folder containing your images.  Images to be displayed will be sorted alphanumerically. 
+# Next we will "find" all the "jpg", "jpeg" or "png" images in your public folder containing your images.  Images to be displayed will be sorted alphanumerically. 
 # Arrange photo file names alphanumerically to determine display order when running this script
 my @find = `find /var/www/YourSite.net/public_html/YourDirectory . -name "*.jpg" -o -name "*.JPG" -o -name "*.jpeg" -o -name "*.JPEG" -o -name "*.png" -o -name "*.PNG" |  sort --ignore-case`;
 
